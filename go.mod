5bc569f8600ff8e359c893eb465bc6cc98d6c78e
Lawrence Jones <lawrjone@gmail.com>
2019-05-22 15:08:54 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/lawrencejones/rebuild-index-concurrently
+
+go 1.12
+
+require (
+	github.com/alecthomas/kingpin v2.2.6+incompatible
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/cloudflare/cfssl v0.0.0-20190510060611-9c027c93ba9e
+	github.com/coreos/etcd v3.3.13+incompatible
+	github.com/davecgh/go-spew v1.1.1
+	github.com/go-kit/kit v0.8.0
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/google/certificate-transparency-go v1.0.21 // indirect
+	github.com/jackc/pgx v3.4.0+incompatible // indirect
+	github.com/montanaflynn/stats v0.5.0
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/grpc v1.20.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net/http/httptrace"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	tlshelpers "github.com/cloudflare/cfssl/helpers"
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/coreos/etcd/clientv3"
+	kitlog "github.com/go-kit/kit/log"
+	"github.com/montanaflynn/stats"
+)
+
+var logger kitlog.Logger
+
+var (
+	app                = kingpin.New("etcd-latency", "").Version("0.0.1")
+	count              = app.Flag("count", "number of benchmark runs").Default("100").Int()
+	endpoints          = app.Flag("endpoints", "comma separated etcd endpoint list").Default("127.0.0.1:2379").Envar(`ETCDCTL_ENDPOINTS`).String()
+	dialTimeout        = app.Flag("dial-timeout", "dial timeout for client connections").Default("5s").Envar(`ETCDCTL_DIAL_TIMEOUT`).Duration()
+	keepaliveTime      = app.Flag("keepalive-time", "keepalive time for client connections").Default("2s").Envar(`ETCDCTL_KEEPALIVE_TIME`).Duration()
+	keepaliveTimeout   = app.Flag("keepalive-timeout", "keepalive timeout for client connections").Envar(`ETCDCTL_KEEPALIVE_TIMEOUT`).Default("6s").Duration()
+	insecure           = app.Flag("insecure", "disable transport security for client connections").Envar(`ETCDCTL_INSECURE`).Bool()
+	insecureSkipVerify = app.Flag("insecure-skip-verify", "accept insecure SRV records describing cluster endpoints").Envar(`ETCDCTL_INSECURE_SKIP_TLS_VERIFY`).Bool()
+	certFile           = app.Flag("cert", "identify secure client using this TLS certificate file").Envar(`ETCDCTL_CERT`).String()
+	keyFile            = app.Flag("key", "identify secure client using this TLS key file").Envar(`ETCDCTL_KEY`).String()
+	cacertFile         = app.Flag("cacert", "verify certificates of TLS-enabled secure servers using this CA bundle").Envar(`ETCDCTL_CACERT`).String()
+)
+
+func main() {
+	logger = kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(os.Stderr))
+	logger = kitlog.With(logger, "ts", kitlog.DefaultTimestampUTC)
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		logger.Log("msg", "received signal, shutting down")
+		cancel()
+	}()
+
+	tlsCfg, err := newTLS(*certFile, *keyFile, *cacertFile, *insecureSkipVerify)
+	if err != nil {
+		kingpin.Fatalf("invalid tls: %v", err)
+	}
+
+	var start time.Time
+	var trace = httptrace.ClientTrace{
+		DNSStart: func(_ httptrace.DNSStartInfo) {
+			logger.Log("trace", "dns_start", "duration", time.Since(start))
+		},
+		DNSDone: func(_ httptrace.DNSDoneInfo) {
+			logger.Log("trace", "dns_done", "duration", time.Since(start))
+		},
+		ConnectDone: func(network, addr string, err error) {
+			logger.Log("trace", "connect_done", "addr", addr, "duration", time.Since(start))
+		},
+		TLSHandshakeStart: func() {
+			logger.Log("trace", "tls_handshake_start", "duration", time.Since(start))
+		},
+		TLSHandshakeDone: func(state tls.ConnectionState, err error) {
+			logger.Log("trace", "tls_handshake_done", "state", state, "duration", time.Since(start))
+		},
+		GotFirstResponseByte: func() {
+			logger.Log("trace", "got_first_byte", "duration", time.Since(start))
+		},
+	}
+
+	ctx = httptrace.WithClientTrace(ctx, &trace)
+	start = time.Now()
+
+	client, err := clientv3.New(
+		clientv3.Config{
+			Context:              ctx,
+			TLS:                  tlsCfg,
+			Endpoints:            strings.Split(*endpoints, ","),
+			DialTimeout:          *dialTimeout,
+			DialKeepAliveTime:    *keepaliveTime,
+			DialKeepAliveTimeout: *keepaliveTimeout,
+		},
+	)
+
+	logger.Log("trace", "connection_established", "duration", time.Since(start))
+
+	if err != nil {
+		kingpin.Fatalf("failed to connect to etcd: %s", err)
+	}
+
+	spew.Dump(
+		runBenchmarks(ctx, *count, map[string]func(context.Context) error{
+			"linearisable get for missing key": func(ctx context.Context) error {
+				_, err := client.Get(ctx, "nothing")
+				return err
+			},
+			"serializable get for missing key": func(ctx context.Context) error {
+				_, err := client.Get(ctx, "nothing", clientv3.WithSerializable())
+				return err
+			},
+		}),
+	)
+}
+
+func runBenchmarks(ctx context.Context, count int, ops map[string]func(context.Context) error) map[string]interface{} {
+	var results = map[string]interface{}{}
+	for name, op := range ops {
+		results[name] = runBenchmark(ctx, count, op)
+	}
+
+	return results
+}
+
+func runBenchmark(ctx context.Context, count int, op func(context.Context) error) interface{} {
+	var errors int
+	var timings = []float64{}
+
+	for i := 0; i < count; i++ {
+		start := time.Now()
+		if err := op(ctx); err != nil {
+			errors++
+		}
+
+		timings = append(timings, float64(time.Since(start)))
+	}
+
+	must := func(res float64, _ error) time.Duration { return time.Duration(res) }
+
+	return struct{ mean, p50, p75, p90, p95 time.Duration }{
+		mean: must(stats.Mean(timings)),
+		p50:  must(stats.Percentile(timings, 50)),
+		p75:  must(stats.Percentile(timings, 75)),
+		p90:  must(stats.Percentile(timings, 90)),
+		p95:  must(stats.Percentile(timings, 95)),
+	}
+}
+
+func newTLS(certFile, keyFile, cacertFile string, skipTLSVerify bool) (*tls.Config, error) {
+	if certFile == "" && keyFile == "" && cacertFile == "" {
+		return nil, nil // no TLS required
+	}
+
+	var cfg = &tls.Config{}
+
+	if certFile != "" && keyFile != "" {
+		cert, err := tlshelpers.LoadClientCertificate(certFile, keyFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load client certs: %s", err)
+		}
+
+		cfg.Certificates = []tls.Certificate{*cert}
+	}
+
+	if cacertFile != "" {
+		roots, err := tlshelpers.LoadPEMCertPool(cacertFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load CA file: %s", err)
+		}
+
+		cfg.RootCAs = roots
+	}
+
+	cfg.InsecureSkipVerify = skipTLSVerify
+
+	return cfg, nil
+}
