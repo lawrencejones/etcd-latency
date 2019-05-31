a51795f4ec88e10e0277a7b2e3ea71f42a375791
Lawrence Jones <lawrjone@gmail.com>
2019-05-31 14:45:07 +0100

    Additional debug logging

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	stdlog "log"
 	"net/http/httptrace"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 	"time"
+
+	"google.golang.org/grpc/grpclog"
 
 	tlshelpers "github.com/cloudflare/cfssl/helpers"
 	"github.com/davecgh/go-spew/spew"
@@ -24,6 +27,8 @@ var logger kitlog.Logger
 
 var (
 	app                = kingpin.New("etcd-latency", "").Version("0.0.1")
+	grpcDebugLogs      = app.Flag("grpc-debug-logs", "enables grpc debug logging").Bool()
+	sleep              = app.Flag("sleep", "sleep for this long after opening connection").Default("0s").Duration()
 	count              = app.Flag("count", "number of benchmark runs").Default("100").Int()
 	endpoints          = app.Flag("endpoints", "comma separated etcd endpoint list").Default("127.0.0.1:2379").Envar(`ETCDCTL_ENDPOINTS`).String()
 	dialTimeout        = app.Flag("dial-timeout", "dial timeout for client connections").Default("5s").Envar(`ETCDCTL_DIAL_TIMEOUT`).Duration()
@@ -36,11 +41,25 @@ var (
 	cacertFile         = app.Flag("cacert", "verify certificates of TLS-enabled secure servers using this CA bundle").Envar(`ETCDCTL_CACERT`).String()
 )
 
+type kitlogWriter struct{ kitlog.Logger }
+
+func (l kitlogWriter) Write(p []byte) (int, error) {
+	return 0, l.Log("msg", string(p))
+}
+
 func main() {
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
 	logger = kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(os.Stderr))
 	logger = kitlog.With(logger, "ts", kitlog.DefaultTimestampUTC)
+	stdlog.SetOutput(kitlog.NewStdlibAdapter(logger))
 
-	kingpin.MustParse(app.Parse(os.Args[1:]))
+	if *grpcDebugLogs {
+		var out = kitlogWriter{logger}
+		clientv3.SetLogger(
+			grpclog.NewLoggerV2WithVerbosity(out, out, out, 0),
+		)
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -67,7 +86,7 @@ func main() {
 			logger.Log("trace", "dns_done", "duration", time.Since(start))
 		},
 		ConnectDone: func(network, addr string, err error) {
-			logger.Log("trace", "connect_done", "addr", addr, "duration", time.Since(start))
+			logger.Log("trace", "connect_done", "addr", addr, "error", err, "duration", time.Since(start))
 		},
 		TLSHandshakeStart: func() {
 			logger.Log("trace", "tls_handshake_start", "duration", time.Since(start))
@@ -100,6 +119,17 @@ func main() {
 		kingpin.Fatalf("failed to connect to etcd: %s", err)
 	}
 
+	if err := client.Sync(ctx); err != nil {
+		kingpin.Fatalf("failed to sync endpoints: %s", err)
+	}
+
+	logger.Log("endpoints", strings.Join(client.Endpoints(), ","))
+
+	if *sleep > 0 {
+		logger.Log("event", "sleeping", "duration", sleep.Seconds())
+		time.Sleep(*sleep)
+	}
+
 	spew.Dump(
 		runBenchmarks(ctx, *count, map[string]func(context.Context) error{
 			"linearisable get for missing key": func(ctx context.Context) error {
@@ -124,13 +154,14 @@ func runBenchmarks(ctx context.Context, count int, ops map[string]func(context.C
 }
 
 func runBenchmark(ctx context.Context, count int, op func(context.Context) error) interface{} {
-	var errors int
+	var errors float64
 	var timings = []float64{}
 
 	for i := 0; i < count; i++ {
 		start := time.Now()
 		if err := op(ctx); err != nil {
 			errors++
+			logger.Log("error", err)
 		}
 
 		timings = append(timings, float64(time.Since(start)))
@@ -138,12 +169,16 @@ func runBenchmark(ctx context.Context, count int, op func(context.Context) error
 
 	must := func(res float64, _ error) time.Duration { return time.Duration(res) }
 
-	return struct{ mean, p50, p75, p90, p95 time.Duration }{
-		mean: must(stats.Mean(timings)),
-		p50:  must(stats.Percentile(timings, 50)),
-		p75:  must(stats.Percentile(timings, 75)),
-		p90:  must(stats.Percentile(timings, 90)),
-		p95:  must(stats.Percentile(timings, 95)),
+	return struct {
+		mean, p50, p75, p90, p95 time.Duration
+		errorRate                float64
+	}{
+		mean:      must(stats.Mean(timings)),
+		p50:       must(stats.Percentile(timings, 50)),
+		p75:       must(stats.Percentile(timings, 75)),
+		p90:       must(stats.Percentile(timings, 90)),
+		p95:       must(stats.Percentile(timings, 95)),
+		errorRate: errors / float64(count),
 	}
 }
 
